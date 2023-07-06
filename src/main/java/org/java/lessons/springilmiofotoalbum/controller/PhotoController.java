83866ff87ad4a995652404e15e6f9a4f5f2c39af
SofiaCurreri <sofiacurreri@gmail.com>
2023-07-06 18:05:25 +0200

    show + create + edit (Photo)

--- src/main/java/org/java/lessons/springilmiofotoalbum/controller/PhotoController.java
+++ src/main/java/org/java/lessons/springilmiofotoalbum/controller/PhotoController.java
@@ -1,5 +1,6 @@
 package org.java.lessons.springilmiofotoalbum.controller;
 
+import jakarta.validation.Valid;
 import org.java.lessons.springilmiofotoalbum.model.Photo;
 import org.java.lessons.springilmiofotoalbum.repository.CategoryRepository;
 import org.java.lessons.springilmiofotoalbum.repository.PhotoRepository;
@@ -7,10 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -45,6 +44,7 @@ public class PhotoController {
         return "index";
     }
 
+    //controller per mostrare dettaglio foto
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Integer photoId, Model model) {
         //.findById gestisce un oggetto di tipo Optional che gestisce anche caso in cui arrivi un Id non sistente
@@ -64,6 +64,58 @@ public class PhotoController {
     public String create(Model model) {
         model.addAttribute("photo", new Photo());
         model.addAttribute("categoryList", categoryRepository.findAll());
-        return "edit"; //template unico per create e edit
+        return "editCreate"; //template unico per create e edit
     }
+
+    @PostMapping("/create")
+    //ci aspettiamo di ricevere un obj di tipo Photo, i cui attributi vengono riempiti dai dati inseriti nel form
+    public String store(@Valid @ModelAttribute("photo") Photo formPhoto, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            //ci sono stati errori
+            model.addAttribute("categoryList", categoryRepository.findAll());
+            return "editCreate"; //ritorno template form ma con foto precaricata
+        }
+
+        //save fa create sql se obj con quella PK non esiste, altrimenti fa update
+        photoRepository.save(formPhoto);
+
+        //se tutto va bene rimando alla lista delle foto
+        return "redirect:/photos";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        //verificare se esiste foto con quell' id
+        Optional<Photo> result = photoRepository.findById(id);
+        if (result.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Photo with id = " + id + " not found :(");
+        }
+        model.addAttribute("photo", result.get());
+        model.addAttribute("categoryList", categoryRepository.findAll());
+        return "editCreate";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@PathVariable Integer id,
+                         @Valid @ModelAttribute("photo") Photo formPhoto,
+                         BindingResult bindingResult,
+                         Model model) {
+        Optional<Photo> result = photoRepository.findById(id);
+        if (result.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Photo with id = " + id + " not found :(");
+        }
+        Photo photoToEdit = result.get(); //vecchia versione foto
+        //nuova versione foto = formPhoto
+
+        //valido formPhoto
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("categoryList", categoryRepository.findAll());
+            return "editCreate";
+        }
+
+        formPhoto.setId(photoToEdit.getId());
+        photoRepository.save(formPhoto);
+        return "redirect:/photos";
+    }
+
 }
