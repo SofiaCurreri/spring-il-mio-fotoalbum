d68aa13ed8cac3a30084417181a77611b3199410
SofiaCurreri <sofiacurreri@gmail.com>
2023-07-07 14:20:18 +0200

    DatabaseUserDetailsService

--- src/main/java/org/java/lessons/springilmiofotoalbum/security/DatabaseUserDetailsService.java
+++ src/main/java/org/java/lessons/springilmiofotoalbum/security/DatabaseUserDetailsService.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class DatabaseUserDetailsService implements UserDetailsService {
 
     //ci serve uno UserRepository per fare query su db sulla tabella users
