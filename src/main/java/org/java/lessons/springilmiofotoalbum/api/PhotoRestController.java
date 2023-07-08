09377198ff84cb34b9d551b7442804052dd0fa25
SofiaCurreri <sofiacurreri@gmail.com>
2023-07-08 17:08:55 +0200

    show + create + delete with RestController

--- src/main/java/org/java/lessons/springilmiofotoalbum/api/PhotoRestController.java
+++ src/main/java/org/java/lessons/springilmiofotoalbum/api/PhotoRestController.java
@@ -3,12 +3,12 @@ package org.java.lessons.springilmiofotoalbum.api;
 import org.java.lessons.springilmiofotoalbum.model.Photo;
 import org.java.lessons.springilmiofotoalbum.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin //permette a client di front-end di accedere
@@ -22,5 +22,29 @@ public class PhotoRestController {
     @GetMapping
     public List<Photo> index() {
         return photoRepository.findAll();
+    }
+
+    //servizio per dettaglio foto
+    @GetMapping("/{id}")
+    public Photo get(@PathVariable Integer id) {
+        Optional<Photo> photo = photoRepository.findById(id);
+        if (photo.isPresent()) {
+            return photo.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //servizio per creare nuova foto; che arriva come Json nel RequestBody
+    @PostMapping
+    //solo POST e PUT hanno Body
+    public Photo create(@RequestBody Photo photo) {
+        return photoRepository.save(photo);
+    }
+
+    //servizio per cancellare foto
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        photoRepository.deleteById(id);
     }
 }
