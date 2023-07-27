288a871403606844ac38ec0e79836ba96cae04b5
SofiaCurreri <sofiacurreri@gmail.com>
2023-07-27 10:58:11 +0200

    Message model

---
+++ src/main/java/org/java/lessons/springilmiofotoalbum/model/Message.java
@@ -0,0 +1,42 @@
+package org.java.lessons.springilmiofotoalbum.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
